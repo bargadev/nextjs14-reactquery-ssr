9d67eb252c5bfc561a53187a2194419a730c94d3
Lucas Barga <lucasmpbarga@gmail.com>
2025-02-27 22:15:30 -0300

    feat: put all in the same place

--- src/more/posts-query.js
+++ src/more/posts-query.js
@@ -1,4 +1,15 @@
-import { getPosts } from "./get-posts";
+export const getPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  const data = await response.json();
+
+  console.log(data);
+
+  return data;
+};
 
 // src/more/posts-query.js
 export const postsQueryConfig = {
